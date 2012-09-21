ab16e607d00857dd186c30b19773b5f97ad6371b
Ali Al-Shabibi <alshabibi.ali@gmail.com>
2012-09-21 12:02:43 -0700

    add run-tests

---
+++ scripts/run-tests.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+REV=$1
+REPO=git@github.com:OPENNETWORKINGLAB/flowvisor-test.git
+
+git clone $REPO
+cd flowvisor-test
+git checkout $REV
+
+cd tools/munger
+make install
+cd ../../tests
+
+./fvt --fv-cmd=../../scripts/flowvisor-emma.sh
+
+
