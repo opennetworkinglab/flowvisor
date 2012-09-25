64b0e5accb854e70b47a82cc4526212ddfa92cee
Ali Al-Shabibi <alshabibi.ali@gmail.com>
2012-09-24 23:25:44 -0700

    returning to jenkins what belongs to jenkins

--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -15,4 +15,4 @@ echo "Making debian package (using sudo)" >&2
 version=`echo $release | sed -e 's/^flowvisor-//'`
 sudo ./scripts/make-deb.sh $version $rtype
 sudo make clean
-chown -R jenkins:nogroup ./scripts/DEB
+sudo chown -R jenkins:nogroup ./scripts/DEB
