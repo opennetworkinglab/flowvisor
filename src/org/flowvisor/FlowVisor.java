bf7ab69569d96425e63f4d31a0bd21f076a747c0
alshabib <alshabibi.ali@gmail.com>
2012-11-27 01:19:46 +0100

    test git tag post build

--- src/org/flowvisor/FlowVisor.java
+++ src/org/flowvisor/FlowVisor.java
@@ -40,7 +40,7 @@ public class FlowVisor {
 	public final static int FLOWVISOR_VENDOR_EXTENSION = 0x80000001;
 
 	// VERSION
-	public final static String FLOWVISOR_VERSION = "flowvisor-0.9.17";
+	public final static String FLOWVISOR_VERSION = "flowvisor-0.9.18";
 	public final static int FLOWVISOR_DB_VERSION = 1;
 
 
