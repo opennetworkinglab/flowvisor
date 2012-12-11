f1f964d724b55dbac2d9688c3b879a4bdfa37d83
alshabib <alshabibi.ali@gmail.com>
2012-12-11 20:24:39 +0100

    tracker bug

--- src/org/flowvisor/message/FVFlowRemoved.java
+++ src/org/flowvisor/message/FVFlowRemoved.java
@@ -38,12 +38,12 @@ public class FVFlowRemoved extends OFFlowRemoved implements Classifiable,
 		FlowMap flowSpace = fvClassifier.getSwitchFlowMap();
 		Set<String> slicesToUpdate = new HashSet<String>();
 		
-		String slicerFromCookie = untanslateCookie(fvClassifier);
+		
 		
 		String sliceName = fvClassifier.getFlowDB().processFlowRemoved(this,
 				fvClassifier.getDPID());
 		
-		
+		String slicerFromCookie = untanslateCookie(fvClassifier);
 		
 		FVLog.log(LogLevel.DEBUG, fvClassifier, "Starting flowremoved message processing");
 		
