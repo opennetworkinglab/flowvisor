ea9e23a6622e16ae4f925880b1bb1aef9f6c5f17
alshabib <alshabibi.ali@gmail.com>
2013-01-10 11:28:46 +0100

    add-slice api

--- src/org/flowvisor/api/JSONRPCService.java
+++ src/org/flowvisor/api/JSONRPCService.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.flowvisor.log.FVLog;
 import org.flowvisor.log.LogLevel;
 
+import com.thetransactioncompany.jsonrpc2.JSONRPC2Error;
 import com.thetransactioncompany.jsonrpc2.JSONRPC2ParseException;
 import com.thetransactioncompany.jsonrpc2.JSONRPC2Request;
 import com.thetransactioncompany.jsonrpc2.JSONRPC2Response;
@@ -32,15 +33,24 @@ public class JSONRPCService {
 
 	public void dispatch(HttpServletRequest req, HttpServletResponse resp) { 
 		JSONRPC2Request json = null;
+		JSONRPC2Response jsonResp = null;
 		try {
 			json = parseJSONRequest(req);
-			JSONRPC2Response jsonResp = dispatcher.process(json, null);
+			jsonResp = dispatcher.process(json, null);
 			jsonResp.setID(json.getID());
+		} catch (IOException e) {
+			jsonResp = new JSONRPC2Response(new JSONRPC2Error(JSONRPC2Error.PARSE_ERROR.getCode(), 
+					e.getMessage()), 0);
+		} catch (JSONRPC2ParseException e) {
+			jsonResp = new JSONRPC2Response(new JSONRPC2Error(JSONRPC2Error.PARSE_ERROR.getCode(), 
+					e.getMessage()), 0);
+		}
+		try {
 			writeJSONObject(resp, jsonResp);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			FVLog.log(LogLevel.CRIT, null, "Unable to send response: " + e.getMessage());
 		}
+		
 		
 	}
 
@@ -51,11 +61,24 @@ public class JSONRPCService {
 	 * @param request the request
 	 * @return the jSON object
 	 * @throws IOException Signals that an I/O exception has occurred.
+	 * @throws JSONRPC2ParseException 
 	 * @throws JSONException the jSON exception
 	 */
 	private JSONRPC2Request parseJSONRequest(HttpServletRequest request)
-	throws IOException{
-		BufferedReader reader = null;
+			throws IOException, JSONRPC2ParseException{
+		BufferedReader reader = request.getReader();
+	    StringBuilder sb = new StringBuilder();
+	    String line = reader.readLine();
+	    while (line != null) {
+	        sb.append(line + "\n");
+	        line = reader.readLine();
+	    }
+	    reader.close();
+	    FVLog.log(LogLevel.DEBUG, null, "---------JSON RPC request: ", sb.toString());
+	    return JSONRPC2Request.parse(sb.toString());
+
+	}
+	/*	BufferedReader reader = null;
 		char[] buff = new char[100];
 		int sz = 0;
 		try {
@@ -73,7 +96,7 @@ public class JSONRPCService {
 				reader.close();
 		}
 
-	}
+	}*/
 	
 	/**
 	 * Write json object.
@@ -83,11 +106,11 @@ public class JSONRPCService {
 	 * @throws IOException Signals that an I/O exception has occurred.
 	 */
 	private void writeJSONObject(HttpServletResponse response, JSONRPC2Response jresp)
-	throws IOException {
+			throws IOException {
 		response.setContentType("text/json; charset=utf-8");
 		String json = jresp.toJSONString();
 		Writer writer = response.getWriter();
-		FVLog.log(LogLevel.DEBUG, null, "---------JSON RPC response:" + json);
+		FVLog.log(LogLevel.DEBUG, null, "---------JSON RPC response:", json);
 		writer.write(json);
 	}
 
