a057d1c86e61e7ec8e47c47b2d18e192e3fc3880
alshabib <alshabibi.ali@gmail.com>
2013-01-10 18:23:00 +0100

    added listflowspace

--- src/org/flowvisor/api/handlers/ListFlowSpace.java
+++ src/org/flowvisor/api/handlers/ListFlowSpace.java
@@ -1,10 +1,13 @@
 package org.flowvisor.api.handlers;
 
+import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.collections.map.HashedMap;
 import org.flowvisor.api.APIUserCred;
 import org.flowvisor.config.ConfigError;
 import org.flowvisor.config.FVConfig;
+import org.flowvisor.config.FlowSpace;
 import org.flowvisor.config.FlowSpaceImpl;
 import org.flowvisor.exceptions.MissingRequiredField;
 import org.flowvisor.exceptions.UnknownFieldType;
@@ -25,15 +28,15 @@ public class ListFlowSpace implements ApiHandler<Map<String, Object>> {
 		try {
 			String sliceName = HandlerUtils.fetchField(SLICENAME, params, String.class, false, null);
 			String user = APIUserCred.getUserName();
-			FlowMap fm = null;
+			HashMap<String, Object> map = new HashMap<String, Object>();
 			if (FVConfig.isSupervisor(user)) {
-				if (sliceName == null)
-					fm = FlowSpaceImpl.getProxy().getFlowMap();
+				if (sliceName == null) 
+					FlowSpaceImpl.getProxy().toJson(map);
 				else
-					fm = FlowSpaceUtil.getSliceFlowSpace(sliceName);
+					FlowSpaceUtil.getSliceFlowSpace(sliceName);
 			} else
-				fm = FlowSpaceUtil.getSliceFlowSpace(user);
-			resp = new JSONRPC2Response(fm.getRules().toArray(), 0);
+				FlowSpaceUtil.getSliceFlowSpace(user);
+			resp = new JSONRPC2Response(map.get(FlowSpace.FS), 0);
 		}  catch (UnknownFieldType e) {
 			resp = new JSONRPC2Response(new JSONRPC2Error(JSONRPC2Error.INVALID_PARAMS.getCode(), 
 					"list-flowspace: " + e.getMessage()), 0);
