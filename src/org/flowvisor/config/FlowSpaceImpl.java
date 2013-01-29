947bf8ceab222cd091ee67589e45c61e26a88ad8
alshabib <alshabibi.ali@gmail.com>
2013-01-28 17:33:47 -0800

    fvctl.py impl

--- src/org/flowvisor/config/FlowSpaceImpl.java
+++ src/org/flowvisor/config/FlowSpaceImpl.java
@@ -30,7 +30,7 @@ public class FlowSpaceImpl implements FlowSpace {
 
 	private ConfDBSettings settings = null;
 	private static FlowSpaceImpl instance =  null;
-	private FlowMap cachedFlowMap = null;
+	//private FlowMap cachedFlowMap = null;
 	
 	
 	//Callbacks
@@ -109,8 +109,8 @@ public class FlowSpaceImpl implements FlowSpace {
 	 */
 	@Override
 	public FlowMap getFlowMap() throws ConfigError {
-		if (cachedFlowMap != null)
-			return cachedFlowMap;
+		/*if (cachedFlowMap != null)
+			return cachedFlowMap;*/
 		Connection conn = null;
 		PreparedStatement ps = null;
 		PreparedStatement actions = null;
@@ -184,7 +184,7 @@ public class FlowSpaceImpl implements FlowSpace {
 			}
 			if (map == null)
 				map = new FederatedFlowMap();
-			cachedFlowMap = map;
+			//cachedFlowMap = map;
 			return map;
 		} catch (SQLException e) {
 			throw new ConfigError("Unable to retrieve flowmap from db : " + e.getMessage());
