c585fd8c6a2972a6975518edfd19923565d2314f
alshabib <alshabibi.ali@gmail.com>
2013-02-05 15:14:04 -0800

    kill jetty logging

--- src/org/flowvisor/api/JettyServer.java
+++ src/org/flowvisor/api/JettyServer.java
@@ -43,7 +43,8 @@ public class JettyServer implements Runnable{
 	private void init(int port){
 
 		//System.setProperty("org.eclipse.jetty.util.log.class", JettyLog.class.getCanonicalName());
-
+		org.eclipse.jetty.util.log.Log.setLog(null);
+		
 		FVLog.log(LogLevel.INFO, null,
 				"initializing FlowVisor UserAPI JSONRPC SSL WebServer on port "
 						+ port);
