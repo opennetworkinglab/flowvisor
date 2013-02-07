3ce5f25bb2412ac3fb1ecb018974d3830839c437
alshabib <alshabibi.ali@gmail.com>
2013-02-07 13:58:58 -0800

    hex out

--- src/org/flowvisor/FlowVisor.java
+++ src/org/flowvisor/FlowVisor.java
@@ -40,7 +40,7 @@ public class FlowVisor {
 	public final static int FLOWVISOR_VENDOR_EXTENSION = 0x80000001;
 
 	// VERSION
-	public final static String FLOWVISOR_VERSION = "flowvisor-0.11.8";
+	public final static String FLOWVISOR_VERSION = "flowvisor-0.11.9";
 	public final static int FLOWVISOR_DB_VERSION = 2;
 
 
