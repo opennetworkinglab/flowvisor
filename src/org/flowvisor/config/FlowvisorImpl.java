e8bedad1ed81236382b437f1b89aa13e8ab27a78
alshabib <alshabibi.ali@gmail.com>
2013-02-07 21:06:21 -0800

    sql state

--- src/org/flowvisor/config/FlowvisorImpl.java
+++ src/org/flowvisor/config/FlowvisorImpl.java
@@ -996,7 +996,7 @@ public class FlowvisorImpl implements Flowvisor {
 				
 		} catch (SQLException e) {
 			System.err.println("Error code " + e.getErrorCode() + " sql state " + e.getSQLState());
-			System.err.println("Updating database failed, exiting : " + e.getMessage());
+			System.err.println("Updating database failed, exiting : " + e.getNextException().getMessage());
 			System.exit(1);
 		} finally {
 			close(ps);
