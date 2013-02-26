be6c72b1b8090a9ce48b76ecee7d15f3e0e45762
Marc De Leenheer <marc@onlab.us>
2013-02-26 10:58:04 -0800

    Create repo for all DEB versions. Build RPMs and repo.

---
+++ scripts/make-deb-repo.sh
@@ -0,0 +1,149 @@
+#!/bin/sh
+
+if [ $# -ne 2 ] ; then
+    echo "Usage: $0 version-string build-type" >&2
+    exit 1
+fi
+if [ ! -f Makefile ] ; then 
+    echo "Run me in the base of the source directory; no Makefile found" >&2
+    exit 1
+fi
+
+if [ `id -u` != 0 ]; then
+    echo "You're not running as root (well, uid=0) so will likely get errors" >&2
+fi
+
+version=$1
+type=$2
+ARCH="all"
+USER="flowvisor"
+GROUP="flowvisor"
+
+base=`pwd`
+rm -rf $base/pkgbuild
+make prefix=/usr root=$base/pkgbuild/root fvuser=$USER fvgroup=$GROUP pkg-install
+
+mkdir -p $base/pkgbuild/root/etc/init.d
+# cp ./scripts/fv-startup.sh $base/pkgbuild/root/etc/init.d/flowvisor
+chmod 755 $base/pkgbuild/root/etc/init.d/flowvisor
+
+mkdir -p $base/pkgbuild/debian
+cd $base/pkgbuild/debian
+
+cat > control << EOF
+Source: flowvisor
+Maintainer: Ali Al-Shabibi <ali.al-shabibi@onlab.us>
+Section: misc
+Priority: optional
+
+Package: flowvisor
+Version: $version
+Architecture: $ARCH
+Description: The OpenFlow FlowVisor
+Depends: openjdk-6-jre-headless, python (>= 2.6), logrotate
+EOF
+
+cat > preinst << EOF
+#!/bin/bash -e
+#
+# summary of how this script can be called:
+#        * <new-preinst> install
+#        * <new-preinst> install <old-version>
+#        * <new-preinst> upgrade <old-version>
+#        * <old-preinst> abort-upgrade <new-version>
+#
+
+. /usr/share/debconf/confmodule
+
+if [ -n "$DEBIAN_SCRIPT_DEBUG" ]; then set -v -x; DEBIAN_SCRIPT_TRACE=1; fi
+${DEBIAN_SCRIPT_TRACE:+ echo "#42#DEBUG# RUNNING $0 $*" 1>&2 }
+
+# creating flowvisor group if he isn't already there
+if ! getent group flowvisor >/dev/null; then
+    # Adding system group: flowvisor.
+    echo "Creating FlowVisor (flowvisor) group."
+    addgroup --system flowvisor >/dev/null
+fi
+
+# creating flowvisor user if he isn't already there
+if ! getent passwd flowvisor >/dev/null; then
+    # Adding system user: flowvisor.
+    echo "Creating FlowVisor (flowvisor) user."
+    adduser \
+      --system \
+          --disabled-login \
+      --ingroup flowvisor \
+      --no-create-home \
+      --home /nonexistent \
+      --gecos "FlowVisor Hypervisor" \
+      --shell /bin/false \
+      flowvisor  >/dev/null
+fi
+EOF
+
+cat > postinst << EOF
+#!/bin/bash -e
+#
+# summary of how this script can be called:
+#        * <new-preinst> install
+#        * <new-preinst> install <old-version>
+#        * <new-preinst> upgrade <old-version>
+#        * <old-preinst> abort-upgrade <new-version>
+#
+
+. /usr/share/debconf/confmodule
+
+if [ -n "$DEBIAN_SCRIPT_DEBUG" ]; then set -v -x; DEBIAN_SCRIPT_TRACE=1; fi
+${DEBIAN_SCRIPT_TRACE:+ echo "#42#DEBUG# RUNNING $0 $*" 1>&2 }
+
+if [ -d /usr/share/db/flowvisor/FlowVisorDB/seg0 ]; then
+    echo "FlowVisorDB exists, leaving untouched."
+else
+    echo "Please run fvconfig generate to create and initialize the database"
+fi
+EOF
+
+cat > changelog << EOF
+flowvisor ($version-1) $type; urgency=low
+
+  * Initial release
+
+ -- Ali Al-Shabibi (FlowVisor) <ali.al-shabibi@onlab.us>  `date --rfc-822`
+
+EOF
+
+cat > rules << EOF
+#!/usr/bin/make -f
+
+%:
+	dh \$@
+EOF
+
+# cat > files << EOF
+# flowvisor_$version-1_amd64.deb misc optional
+# EOF
+
+cat > compat << EOF
+7
+EOF
+
+cat > flowvisor.install << EOF
+root/etc/* etc/
+root/usr/* usr/
+root/var/* var/
+EOF
+
+chmod 775 preinst
+chmod 775 postinst
+chmod 775 rules
+
+cd ..
+
+dpkg-buildpackage -b
+
+cd ..
+
+chown jenkins:nogroup *.deb
+chown jenkins:nogroup *.changes
+#mv *.deb /var/packages/repo/debian/mini-dinstall/incoming
+#mv *.changes /var/packages/repo/debian/mini-dinstall/incoming
---
+++ scripts/make-rpm-repo.sh
@@ -0,0 +1,99 @@
+#!/bin/sh
+
+if [ $# -ne 1 ] ; then
+    echo "Usage: $0 version-string" >&2
+    exit 1
+fi
+if [ ! -f Makefile ] ; then 
+    echo "Run me in the base of the source directory; no Makefile found" >&2
+    exit 1
+fi
+
+if [ `id -u` != 0 ]; then
+    echo "You're not running as root (well, uid=0) so will likely get errors" >&2
+fi
+
+version=$1
+type=$2
+USER="flowvisor"
+GROUP="flowvisor"
+
+base=`pwd`
+# Clean and setup RPM build environment
+mkdir -p $base/pkgbuild/BUILD
+mkdir -p $base/pkgbuild/RPMS
+mkdir -p $base/pkgbuild/SOURCES
+mkdir -p $base/pkgbuild/SPECS
+mkdir -p $base/pkgbuild/SRPMS
+
+rm -rf $base/pkgbuild/BUILDROOT
+if [ ! -d $base/pkgbuild/root ] ; then 
+    make prefix=/usr root=$base/pkgbuild/BUILDROOT/flowvisor-$version-1.x86_64 fvuser=$USER fvgroup=$GROUP pkg-install;
+else
+    mv $base/pkgbuild/root $base/pkgbuild/BUILDROOT/flowvisor-$version-1.x86_64;
+fi
+
+cat > $base/pkgbuild/SPECS/flowvisor.spec << EOF
+# Folder where RPM will be built
+%define _topdir			$base/pkgbuild
+# Disable rpmbuild macros for binary packages
+%global __os_install_post 	%{nil}
+
+Summary:	The OpenFlow FlowVisor
+Name:		flowvisor
+Version:	$version
+Release:	1%{?dist}
+Group:		Applications/Engineering
+URL:		http://www.flowvisor.org
+Packager:	Marc De Leenheer <marc@onlab.us>
+Vendor:		Open Networking Laboratory (ON.Lab)
+BuildArch:	noarch
+BuildRoot:	%{_topdir}/BUILDROOT
+Source:		%{name}-%{version}.tar.gz
+License:	GPL2
+Requires:	java-1.6.0-openjdk, python >= 2.6, logrotate
+
+%description
+An OpenFlow controller that acts as a hypervisor/proxy
+between a switch and multiple controllers.  Can slice
+multiple switches in parallel, effectively slicing a network.
+
+%post
+if [ -d /usr/share/db/flowvisor/FlowVisorDB/seg0 ]; then
+    echo "FlowVisorDB exists, leaving untouched."
+else
+    echo "Please run fvconfig generate to create and initialize the database"
+fi
+
+%files
+%defattr(644, root, root, 755)
+%config /etc/flowvisor
+%attr(755, root, root) /etc/init.d/flowvisor
+%attr(755, root, root) /usr/bin/fvctl
+%attr(755, root, root) /usr/bin/fvctl-xml
+/usr/libexec/flowvisor
+%attr(755, root, root) /usr/sbin/derby-interact
+%attr(755, root, root) /usr/sbin/flowvisor
+%attr(755, root, root) /usr/sbin/fvconfig
+%config /usr/share/db/flowvisor
+%doc /usr/share/doc/flowvisor
+%doc /usr/share/man/man1/fvconfig.1
+%doc /usr/share/man/man1/fvctl.1
+%doc /usr/share/man/man8/flowvisor.8
+/var/log/flowvisor
+%config /etc/logrotate.d/flowvisor
+
+%clean
+rm -r %buildroot
+
+%changelog
+* Tue Feb 20 2013 Marc De Leenheer <marc@onlab.us> $version-1
+- Initial RPM release.
+EOF
+
+# Build binary RPM package (-bb binary only, -bs source only, -ba source and binary)
+rpmbuild -bb $base/pkgbuild/SPECS/flowvisor.spec
+# Sign RPM
+rpm --addsign $base/pkgbuild/RPMS/noarch/flowvisor-$version-1.noarch.rpm
+
+mv $base/pkgbuild/RPMS/noarch/flowvisor-$version-1.noarch.rpm /var/packages/repo/rpm/$type
