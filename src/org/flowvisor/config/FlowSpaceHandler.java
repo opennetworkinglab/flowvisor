51da262c12330103eeeea3e9154d156ea3c4d4a8
alshabib <alshabibi.ali@gmail.com>
2013-04-30 13:53:12 -0700

    forgot file

---
+++ src/org/flowvisor/config/FlowSpaceHandler.java
@@ -0,0 +1,93 @@
+package org.flowvisor.config;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.flowvisor.api.APIUserCred;
+import org.flowvisor.flows.FlowEntry;
+import org.flowvisor.flows.FlowMap;
+import org.flowvisor.flows.FlowSpaceUtil;
+import org.flowvisor.log.FVLog;
+import org.flowvisor.log.LogLevel;
+import org.openflow.protocol.OFMatch;
+import org.openflow.protocol.action.OFAction;
+
+public class FlowSpaceHandler extends ConcurrentHashMap<Integer, List<FlowEntry>> implements Runnable {
+
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private FlowMap flowSpace = null;
+	private AtomicInteger nextId = null;
+	private boolean shutdown;
+	
+	public FlowSpaceHandler() {
+		flowSpace = FVConfig.getFlowSpaceFlowMap();
+		nextId = new AtomicInteger(0);
+		this.shutdown = false;
+	}
+	
+	@Override
+	public void run() {
+		while (!shutdown) {
+			if (!isEmpty()) {
+				
+				Iterator<Entry<Integer, List<FlowEntry>>> it = this.entrySet().iterator();
+				while (it.hasNext()) {
+					Entry<Integer, List<FlowEntry>> entry = it.next();
+					for (FlowEntry fentry : entry.getValue()) {
+						try {
+							FlowSpaceImpl.getProxy().addRule(fentry);
+							flowSpace.addRule(fentry);
+							String logMsg = "User " + APIUserCred.getUserName() + 
+									flowspaceAddChangeLogMessage(fentry.getDpid(), 
+											fentry.getRuleMatch(), fentry.getPriority(),
+											fentry.getActionsList(), fentry.getName());
+							FVLog.log(LogLevel.INFO, null, logMsg);
+							FlowSpaceImpl.getProxy().notifyChange(flowSpace);
+							
+						} catch (ConfigError e) {
+							FVLog.log(LogLevel.WARN, null, e.getMessage());
+						}
+					}
+					it.remove();
+				}
+			}
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				FVLog.log(LogLevel.DEBUG, null, "Shutting down FlowSpace handler; exiting.");
+				break;
+			}
+		}
+		
+	}
+	
+	public Integer add(List<FlowEntry> fes) {
+		int next = nextId.getAndIncrement();
+		this.put(next, fes);
+		return next;
+	}
+	
+	public boolean exists(Integer id) {
+		return this.containsKey(id);
+	}
+	
+	public void shutdown() {
+		this.shutdown = true;
+		
+	}
+	
+	private static String flowspaceAddChangeLogMessage(long dpid, OFMatch match,
+			int priority, List<OFAction> actions, String name ){
+		return " for dpid=" + FlowSpaceUtil.dpidToString(dpid) + " match=" + match +
+		" priority=" + priority + " actions=" + FlowSpaceUtil.toString(actions) + " name=" + name;
+	}
+
+	
+	
+}
